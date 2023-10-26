053abac9dc9b97788d8111d4d07d9ace37277771
Ciara Ni Shiochru <ciarasugrue@gmail.com>
2023-10-26 14:45:01 +0100

    create delivery employee frontend

--- controller/employeeController.ts
+++ controller/employeeController.ts
@@ -15,4 +15,25 @@ module.exports = function (app: Application) {
 
         res.render('list-delivery-employees', { deliveryEmployees: data })
     })
+
+
+app.get('/employee/delivery/create', async (req:Request, res: Response) => {
+    res.render('add-delivery-employee')
+})
+
+app.post('/employee/delivery/create', async (req:Request, res:Response) => {
+    let data: DeliveryEmployee = req.body
+    let id: Number
+
+    try {
+        console.log(data)
+        id = await employeeService.createEmployee(data)
+        res.redirect('/employees/delivery')
+    } catch(e) {
+        console.error(e);
+        res.locals.errormessage = e.message
+        res.render('add-delivery-employee', req.body)
+    }
+})
+
 }
--- services/employeeService.ts
+++ services/employeeService.ts
@@ -2,12 +2,21 @@ import { DeliveryEmployee } from "../model/deliveryEmployee"
 const axios = require('axios');
 
 
-module.exports.getDeliveryEmployees =async function (): Promise<DeliveryEmployee[]> {
+module.exports.getDeliveryEmployees = async function (): Promise<DeliveryEmployee[]> {
     try {
         const response = await axios.get('http://localhost:8080/api/employee/delivery')
 
         return response.data
     } catch (e) {
         throw new Error('Could not get delivery employees')
+    }
+}
+
+module.exports.createEmployee = async function (deliveryEmployee: DeliveryEmployee): Promise<number> {
+    try {
+        const response = await axios.post('http://localhost:8080/api/employee/delivery', deliveryEmployee)
+        return response.data
+    } catch (e) {
+        throw new Error('Could not create product')
     }
 }
