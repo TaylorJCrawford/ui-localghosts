77fc8c85a34ece9ad52e37982817325311c0bd82
Taylor Crawford <tcrawford540@gmail.com>
2023-10-26 12:10:45 +0100

    [004] Remove non required console log

--- controller/employeeController.ts
+++ controller/employeeController.ts
@@ -13,8 +13,6 @@ module.exports = function (app: Application) {
             console.error(e);
         }
 
-        console.log(data)
-
         res.render('list-delivery-employees', { deliveryEmployees: data })
     })
 }
